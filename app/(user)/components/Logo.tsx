702d0266b0cb2c13818a5c9cc64f2bff816cca29
Faheem <faheemk793@gmail.com>
2026-02-02 14:35:54 +0530

    fixed

--- app/(user)/components/Logo.tsx
+++ app/(user)/components/Logo.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
-
+import Link from "next/link";
 const Logo = ({ className }: { className?: string }) => {
   return (
-    <Image
-      src="/logo/logo.png"
-      alt="Logo"
-      width={100}
-      height={100}
-      className={`drop-shadow-md ${className}`}
-    />
+    <Link href={"/"}>
+      <Image
+        src="/logo/logo.png"
+        alt="Logo"
+        width={100}
+        height={100}
+        className={`drop-shadow-md ${className}`}
+      />
+    </Link>
   );
 };
 
